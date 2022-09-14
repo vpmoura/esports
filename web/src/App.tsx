3223451b6b802f6d82775d46df0212e77118b0b3
Vinicius Moura <vinicius-chabe@hotmail.com>
2022-09-13 23:50:15 -0300

    v-1.0.2

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,20 @@
-function App() {
-  return (
-   <h1>Hello NLW!</h1>
-  )
+interface ButtonProps {
+  title: string;
 }
 
-export default App
+function Button(props: ButtonProps) {
+  return <button>{props.title}</button>;
+}
+
+function App() {
+  return (
+    <div>
+      <Button title="Send 1" />
+      <Button title="Send 2" />
+      <Button title="Send 3" />
+      <Button title="Hello World" />
+    </div>
+  );
+}
+
+export default App;
