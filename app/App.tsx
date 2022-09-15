0e23693a7a4d49bb144a62135fa8bbdccb43d8b4
Vinicius Moura <vinicius-chabe@hotmail.com>
2022-09-15 19:25:52 -0300

    v-1.0.4

--- app/App.tsx
+++ app/App.tsx
@@ -1,10 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity,} from "react-native";
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button (props: ButtonProps){
+  return (
+    <TouchableOpacity>
+      <Text>{props.title}</Text>
+    </TouchableOpacity>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title="Daniel Bumbum Guloso" />
+      <Button title="Sor Gostigui" />
+      <Button title="VGaymer" />
+      <Button title="Henrique Gado" />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
