218ceef56213083383f24888876bd27ce5e1af64
Vinicius Moura <vinicius-chabe@hotmail.com>
2022-09-16 20:21:14 -0300

    v-1.0.6

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,8 +4,35 @@ import logoImg from "./assets/logo-nlw-esports.png";
 
 function App() {
   return (
-    <div className="">
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="" />
+      <h1 className="text-6xl text-white font-black">
+        O seu{" "}
+        <span className="text-transparent bg-nlw-gradient bg-clip-text">
+          duo
+        </span>{" "}
+        está aqui
+      </h1>
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="">
+          <img src="/game-1.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-2.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-3.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-4.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-5.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-6.png" alt="" />
+        </a>
+      </div>
     </div>
   );
 }
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,6 +5,8 @@ module.exports = {
     extend: {
       backgroundImage: {
         galaxy: "url('./public/background-galaxy@3x.png')",
+        "nlw-gradient":
+          "linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 40.94%, #E1D55D 33.57%)",
       },
     },
   },
