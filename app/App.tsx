be281d9f3464fe1051726bc4f16a802351ae5822
Vinicius Moura <vinicius-chabe@hotmail.com>
2022-09-19 17:51:22 -0300

    v-1.1.0

--- app/App.tsx
+++ app/App.tsx
@@ -1,35 +1,14 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TouchableOpacity,} from "react-native";
-
-interface ButtonProps {
-  title: string;
-}
-
-function Button (props: ButtonProps){
-  return (
-    <TouchableOpacity>
-      <Text>{props.title}</Text>
-    </TouchableOpacity>
-  )
-}
+import { StatusBar } from "react-native";
+import { Background } from "./src/components/Background";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Button title="Daniel Bumbum Guloso" />
-      <Button title="Sor Gostigui" />
-      <Button title="VGaymer" />
-      <Button title="Henrique Gado" />
-      <StatusBar style="auto" />
-    </View>
+    <Background>
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor="transparent"
+        translucent
+      />
+    </Background>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
